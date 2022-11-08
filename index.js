fa2e8c5bd851ed7c25007109cce24f553429700d
KamalEmon <u1304013@student.cuet.ac.bd>
2022-11-08 10:24:17 +0600

    Mongodb connected

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
 const app = express()
 const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = "mongodb+srv://process.env.DB_USER:process.env.DB_PASSWORD@cluster0.ph4ajav.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 app.get('/', (req, res) => {
   res.send(`Welcome to Roza's flavor fusion`)
